dad4916b0626f1a4edd4666cb66585b3b934fe96
Oghenedoro David Agbroko <sirdoro1@gmail.com>
2025-10-01 23:25:58 +0100

    Initial Push With Day 1 already Delivered

---
+++ src/pages/about/index.tsx
@@ -0,0 +1,9 @@
+// src/pages/about/index.tsx
+export default function About()
+{
+    // Day 1 of 30 Days Code Snippet Challenge
+    return <div>
+           <h1>About Page</h1>
+           <p>This is the about page</p>
+    </div>
+}
