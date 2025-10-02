df31d0cfe89461236bc94875bfc688f5706b5137
Oghenedoro David Agbroko <sirdoro1@gmail.com>
2025-10-03 00:58:55 +0100

    Update on day-2

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,19 @@
 // src/pages/index.tsx
+
+import Link from 'next/link'
+
 export default function Home(){
-  // Day 1 of 30 Days Code Snippet
+  // Day 2 of 30 Days Code Snippet
   return <div>
-    <h1 >Hello World</h1>
-    <p>Welcome to my 30Days Code Snippet Challenge</p>
+      <h1 style={{marginTop: '20px', textAlign:'center'}}>
+        Welcome to our Home Page
+      </h1>
+      <p style={{marginTop: '20px'}}>
+        Home Page - Routing and Navigation in Next.js
+      </p>
+      <div style={{marginTop: '20px'}}>
+        <Link href={'/about'} style={{margin:'5px'}}> About  Page </Link>
+        <Link href={'/contact'}> Contact Page</Link>
+    </div>
   </div>
 }
